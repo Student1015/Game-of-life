f178614e97bc64382be73afe85dcd132436245d6
Student1015 <student1015@tumo.world>
2022-09-19 18:15:12 +0200

    wefg

---
+++ script.js
@@ -0,0 +1,10 @@
+var clickCount = 0;
+function clickHandler(evt){
+   clickCount++;
+   console.log(evt);
+   var str = "Thanks for clicking " + clickCount;
+   this.innerText = str;
+}
+
+var p = document.getElementById("pElement");
+p.addEventListener("click", clickHandler);
