d0429b3a0cf033bd65b0a6704c202c1e25492069
Student1015 <student1015@tumo.world>
2022-09-21 18:44:22 +0200

    lk

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-var clickCount = 0;
-function clickHandler(evt){
-   clickCount++;
-   console.log(evt);
-   var str = "Thanks for clicking " + clickCount;
-   this.innerText = str;
+function main() {
+    var socket = io();
+    var chatDiv = document.getElementById('Chat');
+    var input = document.getElementById('Nachricht');
+    var button = document.getElementById('Senden');
+ 
+    function handleSubmit(evt) {
+        var val = input.value;
+        if (val != "") {
+            socket.emit("send message", val);
+        }
+    }
+    button.onclick = handleSubmit;
+ 
+    function handleMessage(msg) {
+        var p = document.createElement('p');
+        p.innerText = msg;
+        chatDiv.appendChild(p);
+        input.value = "";
 }
 
-var p = document.getElementById("pElement");
-p.addEventListener("click", clickHandler);
+socket.on('display message', handleMessage);
+} // main closing bracket
 
-function bodyClick(evt){
-    console.log("Clicked at X: " + evt.pageX + ", Y: " + evt.pageY);
- }
- window.onclick = bodyClick;
- //This is the same as window.addEventListener("click", bodyClick);
+window.onload = main;   
+
+ 
